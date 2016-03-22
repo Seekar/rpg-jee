4fcaac68500e1038e9f86c8649a07d61c856890a
Léo Gouttefarde <leogouttefarde@users.noreply.github.com>
2016-03-22 22:45:46 +0100

    Update install_bd.sql

--- install_bd.sql
+++ install_bd.sql
@@ -1,125 +1,140 @@
 
+
+
+drop table Paragraphe;
+drop table Episode;
+drop table Participe;
+drop table Aventure cascade constraints;
+drop table Personnage cascade constraints;
+drop table Biographie;
+drop table Joueur;
+drop table Univers;
+
+
 create table Aventure (
-    id bigint not null,
+    id int not null,
     aDate varchar(255) not null,
     events varchar(255) not null,
-    finie bit not null,
+    finie int not null,
     lieu varchar(255) not null,
     situation varchar(255) not null,
     titre varchar(255) not null,
-    mj_id bigint not null,
-    univers_id bigint not null,
+    mj_id int not null,
+    univers_id int not null,
     primary key (id)
 );
 
 create table Biographie (
-    id bigint not null,
+    id int not null,
     texte varchar(255) not null,
     primary key (id)
 );
 
 create table Episode (
-    id bigint not null,
+    id int not null,
     eDate integer not null,
-    valid bit not null,
-    aventure_id bigint,
-    biographie_id bigint not null,
+    valid int not null,
+    aventure_id int,
+    biographie_id int not null,
     primary key (id)
 );
 
 create table Joueur (
-    id bigint not null,
+    id int not null,
     pseudo varchar(255) not null,
     primary key (id)
 );
 
 create table Paragraphe (
-    id bigint not null,
-    secret bit not null,
+    id int not null,
+    secret int not null,
     texte varchar(255) not null,
-    episode_id bigint not null,
+    episode_id int not null,
     primary key (id)
 );
 
 create table Personnage (
-    id bigint not null,
+    id int not null,
     naissance varchar(255) not null,
     nom varchar(255) not null,
     portrait varchar(255),
     profession varchar(255) not null,
-    valid bit not null,
-    biographie_id bigint,
-    joueur_id bigint not null,
-    transfert_id bigint,
-    univers_id bigint not null,
+    valid int not null,
+    biographie_id int,
+    joueur_id int not null,
+    transfert_id int,
+    univers_id int not null,
     primary key (id)
 );
 
-create table Personnage_Aventure (
-    personnages_id bigint not null,
-    aventures_id bigint not null,
+create table Participe (
+    personnages_id int not null,
+    aventures_id int not null,
     primary key (personnages_id, aventures_id)
 );
 
 create table Univers (
-    id bigint not null,
+    id int not null,
     nom varchar(255) not null,
     primary key (id)
 );
 
+
 alter table Aventure 
-    add constraint FK74E9C5328ACB2B0F 
+    add constraint Aventure_Joueur_FK
     foreign key (mj_id) 
     references Joueur;
 
 alter table Aventure 
-    add constraint FK74E9C5325D6E559A 
+    add constraint Aventure_Univers_FK
     foreign key (univers_id) 
     references Univers 
     on delete cascade;
 
 alter table Episode 
-    add constraint FK72A55DB74088A9A 
+    add constraint Episode_Biographie_FK
     foreign key (biographie_id) 
     references Biographie;
 
 alter table Episode 
-    add constraint FK72A55DB619C649A 
+    add constraint Episode_Aventure_FK 
     foreign key (aventure_id) 
     references Aventure;
 
 alter table Paragraphe 
-    add constraint FK889B40D767F703BA 
+    add constraint Paragraphe_Episode_FK
     foreign key (episode_id) 
     references Episode;
 
 alter table Personnage 
-    add constraint FK9F513EC64E47F43 
+    add constraint Personnage_Transfert_FK
     foreign key (transfert_id) 
     references Joueur;
 
 alter table Personnage 
-    add constraint FK9F513EC65D6E559A 
+    add constraint Personnage_Univers_FK
     foreign key (univers_id) 
     references Univers 
     on delete cascade;
 
 alter table Personnage 
-    add constraint FK9F513EC674088A9A 
+    add constraint Personnage_Biographie_FK
     foreign key (biographie_id) 
     references Biographie;
 
 alter table Personnage 
-    add constraint FK9F513EC6D3CA809A 
+    add constraint Personnage_Joueur_FK
     foreign key (joueur_id) 
     references Joueur;
 
-alter table Personnage_Aventure 
-    add constraint FK4050F68BDEA896D3 
+alter table Participe 
+    add constraint Possede_Personnage_FK
     foreign key (personnages_id) 
     references Personnage;
 
-alter table Personnage_Aventure 
-    add constraint FK4050F68B1E9405AB 
+alter table Participe 
+    add constraint Possede_Aventure_FK
     foreign key (aventures_id) 
     references Aventure;
+
+
