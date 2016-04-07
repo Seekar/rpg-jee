b26e2103edf8032a803e4e2b7b988f50ad5ef09a
Salim Aboubacar <salim.aboubacar@ensimag.grenoble-inp.fr>
2016-04-07 23:57:20 +0200

    DAO et modèle de participe

---
+++ projet/src/main/java/modele/Participe.java
@@ -0,0 +1,59 @@
+package modele;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Participe
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public Aventure aventure;
+        
+        public Aventure getAventure() {
+            return aventure;
+        }
+        public void setAventure(Aventure aventure) {
+            this.aventure = aventure;
+        }
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public Personnage personnage;
+        
+        public Personnage getPersonnage() {
+            return personnage;
+        }
+        public void setPersonnage(Personnage personnage) {
+            this.personnage = personnage;
+        }
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+        
+    public Participe(Aventure aventure, Personnage personnage) {
+        this.aventure = aventure;
+        this.personnage = personnage;
+    }
+
+
+}
+
